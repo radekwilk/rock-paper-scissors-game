f173e449df107a41f6d3884e844a1ff604ac14f4
Radek Wilk <radek.wilk@yahoo.co.uk>
2021-04-02 01:43:52 +0100

    Code has been simplified. Spelling mistakes corrected.

--- game.py
+++ game.py
@@ -1,59 +1,46 @@
 import random
 
 myList = ['rock', 'paper', 'scissors']
-humanWins = False
 
+while True:
+    # getting input from the user
+    user_input = input(
+        'Ready to play game? What is your choice? Type rock, paper or scissors: ')
+    user_input = user_input.lower()  # input to lower case
+    computer_choice = random.choice(myList)  # random selection by computer
 
-def playGame():
-    humanChoice = input(
-        'Ready to play game? What is your choice? Type rock, paper or scissors. ')
-    computerChoice = random.choice(myList)
-    result = [humanChoice.lower(), computerChoice]
-    return result
-
-
-while (humanWins) == False:
-    result = playGame()  # geting user input and random choice of the computer
-    userImput = result[0]  # user choice
-    compChoice = result[1]  # randomly selected by computer
-
-    # if user enter anuthing else than approved rock, paper or rock is asked to enter it again
-    if userImput != 'rock' and userImput != 'paper' and userImput != 'scissors':
+    # if user enter anuthing else than approved rock, paper or scissors is asked to enter it again
+    if user_input != 'rock' and user_input != 'paper' and user_input != 'scissors':
         print(
-            f'Your choice is {userImput}. You can only enter rock, paper or scissors.Please try again!')
+            f'Your choice is {user_input}. You can only enter rock, paper or scissors. Please try again!')
         continue
     # if user enter correct string (rock, paper of scissor), checking who got what and decides how wins. If computer wins, is asking to play again. If user wins game is over
     else:
-        if userImput == compChoice:
+        if user_input == computer_choice:
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. It is a draw. Try again!')
+                f'Your choice is {user_input}. Computer got {computer_choice}. It is a draw. Try again!')
             continue
-        elif userImput == 'rock' and compChoice == 'paper':
+        elif user_input == 'rock' and computer_choice == 'paper':
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. Computer wins!!!. Try again!')
+                f'Your choice is {user_input}. Computer got {computer_choice}. Computer wins!!! Try again!')
             continue
-        elif userImput == 'rock' and compChoice == 'scissors':
+        elif user_input == 'rock' and computer_choice == 'scissors':
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. You won!!!')
-            humanWins = True
+                f'Your choice is {user_input}. Computer got {computer_choice}. You won!!!')
             break
-        elif userImput == 'paper' and compChoice == 'rock':
+        elif user_input == 'paper' and computer_choice == 'rock':
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. You won!!!')
-            humanWins = True
+                f'Your choice is {user_input}. Computer got {computer_choice}. You won!!!')
             break
-        elif userImput == 'paper' and compChoice == 'scissors':
+        elif user_input == 'paper' and computer_choice == 'scissors':
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. Computer wins!!!. Try again!')
-            humanWins = False
+                f'Your choice is {user_input}. Computer got {computer_choice}. Computer wins!!! Try again!')
             continue
-        elif userImput == 'scissors' and compChoice == 'rock':
+        elif user_input == 'scissors' and computer_choice == 'rock':
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. Computer wins!!!. Try again!')
-            humanWins = False
+                f'Your choice is {user_input}. Computer got {computer_choice}. Computer wins!!! Try again!')
             continue
-        elif userImput == 'scissors' and compChoice == 'paper':
+        elif user_input == 'scissors' and computer_choice == 'paper':
             print(
-                f'Your choice is {userImput}. Computer chose {compChoice}. You won!!!')
-            humanWins = True
+                f'Your choice is {user_input}. Computer got {computer_choice}. You won!!!')
             break
